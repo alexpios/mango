d2934724fa6ec78efb4ef2fa7e3fb9799e7b7796
kuchinsky213@gmail.com <244858q>
2018-08-18 17:32:41 +0300

    init1

--- app/src/main/java/by/kuchinsky/alexandr/mangofit/MainActivity.java
+++ app/src/main/java/by/kuchinsky/alexandr/mangofit/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
 Button btnSingUp, btnSignIn;
 TextView slogan;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
